7353ac7922af80bb042a8914e09c095e12598dc6
Amaad <amaad@Amaads-MacBook-Pro.local>
2019-09-06 20:45:18 -0400

    added reverse name and prevent reload. TODO: jumbo

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 $(function () {
     $('#nameform').on('submit', function(evt){
-        alert('nameform clicked');
+        // Reverse name + popup
+        var name = $('#fname').val() + " " + $('#lname').val();
+        alert(name.split("").reverse().join(""));
+
+        // display jumbotron - fix this
+        $('#jumbotext').text(name);
+        $('#jtest').css("display","inline");
+        $('#jtest').show();
+
+        // prevent page reload on submit
+        evt.preventDefault();
       });
+
+
 });
